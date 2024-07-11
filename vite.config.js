0af1c534d95dc8d7015636273bbd5bce3ea03b40
davibcavalcante <davib506@gmail.com>
2024-07-10 21:06:02 -0300

    Arrumando a configuração do VITE para receber variáveis de ambiente

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/portfolio',
   plugins: [react()],
+  define: {
+    'process.env.VITE_IP_API_CONFIG': JSON.stringify(process.env.VITE_IP_API_CONFIG)
+  }
 })
